36ec85486514bec3f1b2f856dbd6065ac34e52bc
Christian Raden <christianraden1120@gmail.com>
2025-06-26 12:59:10 +0800

    Deploy

--- client/src/hooks/useApi.js
+++ client/src/hooks/useApi.js
@@ -8,9 +8,11 @@ const useApi = () => {
   const { getAccessTokenSilently } = useAuth0();
 
   const api = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "https://job-work.onrender.com",
   });
 
+  // https://job-work.onrender.com
+  // http://localhost:5000/api/v1
   // Add auth token to requests
   api.interceptors.request.use(async (config) => {
     try {
